5fa7b000944df685436101ded5919f040c7fac2f
satyam00043 <satyam00043@gmail.com>
2023-11-09 21:18:47 +0530

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './core/layout/footer/footer.component';
+import { HeaderComponent } from './core/layout/header/header.component';
+import { AuthComponent } from './core/auth/auth.component';
+import { ArticleCommentComponent } from './core/feature/article/article-comment/article-comment.component';
+import { ArticleComponent } from './core/feature/article/article/article.component';
+import { EditorComponent } from './feature/editor/editor.component';
+import { HomeComponent } from './feature/home/home.component';
+import { ProfileComponent } from './feature/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    AuthComponent,
+    ArticleCommentComponent,
+    ArticleComponent,
+    EditorComponent,
+    HomeComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
